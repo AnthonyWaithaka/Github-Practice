edc4b8c8239270467a3f39b1f45fdd7cce9ef4ec
AnthonyWaithaka <athumbi1@gmail.com>
2021-09-26 19:17:10 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+// Init app
+const app = express();
+
+let banned_users = [
+    { "username": "spongebob" },
+    { "username": "patrick" },
+    { "username": "xino" },
+    { "username": "squidward" }
+]
+
+// Home route
+app.get('/', (req, res) => {
+    res.send({ "one": "weeeee!" })
+});
+
+app.get('/banned-users', (req, res) => {
+    res.send(banned_users)
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
